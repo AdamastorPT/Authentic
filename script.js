9986a067af92274e7f4df26dd8bc2446512e12bd
António Eloi <36843709+antonioeloi@users.noreply.github.com>
2023-06-04 18:16:10 +0100

    Improve carousel using set interval after html load and improve style of the main index page

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const marcasButton = document.querySelector('#navbar li:nth-child(2)');
-    const submenu = document.querySelector('.submenu');
+  const marcasButton = document.querySelector('#navbar li:nth-child(2)');
+  const submenu = document.querySelector('.submenu');
 
-    marcasButton.addEventListener('mouseenter', () => {
-        submenu.style.display = 'block';
-    });
+  marcasButton.addEventListener('mouseenter', () => {
+      submenu.style.display = 'block';
+  });
 
-    marcasButton.addEventListener('mouseleave', () => {
-        submenu.style.display = 'none';
-    });
-});
-
+  marcasButton.addEventListener('mouseleave', () => {
+      submenu.style.display = 'none';
+  });
 
 
-let count = 1;
-document.getElementById("radio1").checked = true;
+  // Definition of index variable to control loop over the carousel images
+  let index = 1;
 
-setInterval( function(){
-    nextImage();
-}, 2000);
+  // Definiton of number of slides
+  let nSlides = 3;
 
-function nextImage() {
-    count++;
-    if(count>3){
-        count = 1;
+  // Fetch radioIndex button using the ID of the button and change attribute checked to true
+  // This sets the first element of our carousel as the current selected slide
+  document.getElementById("radio" + index).checked = true;
+
+  // Set a method to be executed in a specific interval in milliseconds
+  let interval = 2500;
+  const intervalID = setInterval(nextCarousel, interval);
+
+  // Definition of the nextCarousel method that is called by the setInterval above
+  function nextCarousel () {
+
+    // Increment the variable that controls the loop over the carousel images
+    // This is used to select the next slide
+    index = index + 1;
+
+    // When we reach the end of the carousel we go back to the first element of carousel
+    if (index > nSlides) {
+      index = 1;
     }
-    document.getElementById("radio"+count).checked = true;
-}
+
+    // Fetch radioIndex button using the ID of the button and change attribute checked to true
+    // This sets the index positon element of our carousel as the current selected slide
+    document.getElementById("radio" + index).checked = true;
+  }
+});
